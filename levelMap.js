3de9225213ea30c8618187abecafade05573ee9b
coco-ball <kidbean02@snu.ac.kr>
2023-06-24 01:40:54 +0900

    remove road names

--- levelMap.js
+++ levelMap.js
@@ -4,7 +4,7 @@ function levelMap(element) {
   const map = new mapboxgl.Map({
     container: "map", // container ID
     // style: "mapbox://styles/mapbox/light-v11", // style URL
-    style: "mapbox://styles/yeon8/cliohy76e006j01pwd1fqhuu6", // style URL
+    style: "mapbox://styles/yeon8/clj8ss4qx002p01rchqf3fc6w", // style URL
     center: [126.953527, 37.480439],
     zoom: 15.42,
     pitch: 42,
